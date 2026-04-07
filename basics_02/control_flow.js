02242274fdcd550dcd02b81a0b7c35709f342d4e
VAIBHAV GARG <vaibhgarg5@gmail.com>
2026-04-07 11:01:42 +0000

    update control_flow.js

--- basics_02/control_flow.js
+++ basics_02/control_flow.js
@@ -10,6 +10,10 @@
 // switch is also a conditional statement that allows you to execute different blocks of code based on the value of a variable or expression.
 
 
+
+
+//++++++++++++++++++ if...else if...else statement ++++++++++++++++++
+
 //The if statement evaluates a condition and executes the block of code if the condition is true.
 //The else if statement allows you to specify a new condition to test if the previous condition was false.
 //The else statement executes a block of code if all previous conditions were false.
@@ -21,7 +25,6 @@ if(true) {
 if (false) {
     console.log("This will never execute.");
 }
-
 
 
 //Example of if...else if...else statement
@@ -37,7 +40,7 @@ if (age < 18) {
 
 
 
-
+// ++++++++++++++++++ switch statement ++++++++++++++++++
 
 //switch statement is used to perform different actions based on different conditions.
 let day = 3;
@@ -74,10 +77,58 @@ switch (day) {
 
 
 
+// ++++++++++++++++++ break and continue statements ++++++++++++++++++
+
+//break statement is used to exit a loop or switch statement.
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        break; // exits the loop when i is 5
+    }
+    console.log(i); 
+}
+// Output:
+// 0
+// 1
+// 2
+// 3
+// 4
+// - because the loop is exited when i is 5, so it does not print 5 and the numbers after 5.
+
+
+
+
+//continue statement is used to skip the current iteration of a loop and continue with the next iteration.
+for (let j = 0; j < 10; j++) {
+    if (j === 5) {
+        continue; // skips the current iteration when j is 5
+    }
+    console.log(j); 
+}
+// Output:
+// 0
+// 1
+// 2
+// 3
+// 4
+// 6
+// 7
+// 8
+// 9
+// - because when j is 5, the continue statement skips the rest of the loop body for that iteration, so it does not print 5, 
+// but it continues with the next iterations (6, 7, 8, and 9).
+
+
+
+
+
+
+
 //2. Loops
 //Loops allow you to execute a block of code repeatedly as long as a specified condition is true. 
 //The most common loops in JavaScript are for, while, and do...while.
 
+
+//++++++++++++++++++ for loop ++++++++++++++++++
 
 //For loop is used when the number of iterations is known beforehand. 
 // It consists of three parts: initialization, condition, and increment/decrement.
@@ -85,6 +136,9 @@ for (let i = 0; i < 5; i++) {
     console.log("Iteration: " + i);
 }
 
+
+
+//++++++++++++++++++ while loop ++++++++++++++++++
 
 //While loop is used when the number of iterations is not known and 
 // the loop needs to continue until a certain condition is met.
@@ -94,6 +148,8 @@ while (j < 5) {
     j++;
 }
 
+
+//++++++++++++++++++ do...while loop ++++++++++++++++++
 
 //Do...While loop is similar to the while loop, but it guarantees that the block of code will be executed at least once, 
 // even if the condition is false at the beginning.
